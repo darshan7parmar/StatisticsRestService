fba7f37c6a5fb9e47a13acd3300d673808f5e4ed
Darshan R Parmar <dparmar@opentext.com>
2018-05-05 18:45:00 +0530

    adding the request validation for transaction api

--- src/main/java/api/rest/controller/TransactionController.java
+++ src/main/java/api/rest/controller/TransactionController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 /*
 controller to add new transaction
@@ -17,7 +18,6 @@ controller to add new transaction
 @RestController
 public class TransactionController {
 
-    private static final String template = "Hello, %s!";
     @Autowired
     private TransactionService transactionService;
 
@@ -25,7 +25,7 @@ public class TransactionController {
     @PostMapping
     @RequestMapping(value = "/transactions", consumes = "application/json")
     @ResponseBody
-    public ResponseEntity postData(@RequestBody @Valid Transaction transaction) {
+    public ResponseEntity postData(@RequestBody @Valid @NotNull Transaction transaction) {
         try {
             transactionService.addTransaction(transaction);
             return new ResponseEntity(HttpStatus.CREATED);
--- src/main/java/api/rest/entity/Transaction.java
+++ src/main/java/api/rest/entity/Transaction.java
@@ -6,11 +6,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
-public class Transaction {
 
+public class Transaction {
+    @NotNull
     private final Double amount;
+    @NotNull
     private final Long timestamp;
 
     @JsonCreator
